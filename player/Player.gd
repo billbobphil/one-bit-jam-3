8c1f1a129c48df6e7d31c9db8d8b5ca4a537a0ae
billbobphil <37590467+billbobphil@users.noreply.github.com>
2024-04-05 19:38:00 -0700

    Player refactored to work with single input control

--- player/Player.gd
+++ player/Player.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+
+@onready var sprite : Sprite2D = $Sprite2D;
+@export var canRiseColor : Color = Color(0, 1, 0, 1);
+@export var isFallingColor : Color = Color(1, 0, 0, 1);
+
 @export_category("Curve Speeds")
 @export_subgroup("Vertical")
 @export var riseSpeed : float = 300;
@@ -20,6 +25,7 @@ extends Node2D
 @export var numberOfDiveTicks = 10;
 @export var minimumNumberOfCurveTicks = 5;
 @export var allowedTooSlowTicks = 25;
+@export var minimumLevelOutTicksBeforeCanRise = 20;
 @export_subgroup("Too Slow")
 @export var tooSlowRate : float = 0.025;
 
@@ -29,43 +35,32 @@ var numberOfVerticalFallOffOccurences = 0;
 var numberOfHorizontalFallOffOccurences = 0;
 var currentVerticalFallSpeed;
 var currentHorizontalFallSpeed;
+var leveledOutTicks = 0;
 
 var numberOfPostLevelOutVerticalTicks = 0;
 var verticalTicksBeforeLevelOut = 0;
 
 var hasLeftGround = false;
 var shouldFall = false;
+var canRise = true;
+var isRising = false;
 
 var wentToSlow = false;
 var shouldLevelOut = false;
 
+func _ready():
+	sprite.modulate = canRiseColor;
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
-	if(Input.is_action_pressed("rise")) :
-		position += Vector2(0, -riseSpeed) * delta;
-		hasLeftGround = true;
-		shouldFall = false;
-		wentToSlow = false;
-		tooSlowTicks = 0;
-		timeFalling = 0;
-		numberOfVerticalFallOffOccurences = 0;
-		numberOfHorizontalFallOffOccurences = 0;
-		numberOfPostLevelOutVerticalTicks = 0;
-		shouldLevelOut = false;
-		verticalTicksBeforeLevelOut = 0;
-
-
-	if(Input.is_action_just_released("rise")):
-		shouldFall = true;
-		currentVerticalFallSpeed = fallSpeedVertical;
-		currentHorizontalFallSpeed = fallSpeedHorizontal;
-
 	if(wentToSlow):
+		print('too slow')
 		position += Vector2(crashHorizontalSpeed, crashVerticalSpeed) * delta;
 	elif(shouldFall):
-
-		if(!shouldLevelOut && Input.is_action_just_pressed("levelOut")):
+		print('falling');
+		if(!shouldLevelOut && Input.is_action_just_pressed("toggle")):
+			print('trigger level out');
 			shouldLevelOut = true;
 			verticalTicksBeforeLevelOut = numberOfVerticalFallOffOccurences;
 
@@ -93,7 +88,11 @@ func _process(delta):
 				if(numberOfPostLevelOutVerticalTicks <= verticalTicksBeforeLevelOut + minimumNumberOfCurveTicks):
 					currentHorizontalFallSpeed = currentHorizontalFallSpeed + fallSpeedHorizontalIncrementRate;
 				else:
+					leveledOutTicks += 1;
 					currentHorizontalFallSpeed = currentHorizontalFallSpeed - fallSpeedHorizontalFallOff;
+					if(leveledOutTicks > minimumLevelOutTicksBeforeCanRise):
+						canRise = true;
+						sprite.modulate = canRiseColor;
 
 			if(currentHorizontalFallSpeed < 5):
 				currentHorizontalFallSpeed = 2;
@@ -106,4 +105,35 @@ func _process(delta):
 				wentToSlow = true;
 
 		position += Vector2(currentHorizontalFallSpeed, currentVerticalFallSpeed) * delta;
+
+	if(isRising):
+		print('rising');
+		position += Vector2(0, -riseSpeed) * delta;
+
+	if(isRising && Input.is_action_just_pressed("toggle")):
+		print('trigger falling')
+		canRise = false;
+		isRising = false;
+		shouldFall = true;
+		currentVerticalFallSpeed = fallSpeedVertical;
+		currentHorizontalFallSpeed = fallSpeedHorizontal;
+		sprite.modulate = isFallingColor;
+		
+	if(canRise && !isRising && Input.is_action_just_pressed("toggle")):
+		print('trigger rising')
+		# position += Vector2(0, -riseSpeed) * delta;
+		isRising = true;
+		hasLeftGround = true;
+		shouldFall = false;
+		wentToSlow = false;
+		tooSlowTicks = 0;
+		timeFalling = 0;
+		leveledOutTicks = 0;
+		numberOfVerticalFallOffOccurences = 0;
+		numberOfHorizontalFallOffOccurences = 0;
+		numberOfPostLevelOutVerticalTicks = 0;
+		shouldLevelOut = false;
+		verticalTicksBeforeLevelOut = 0;
+
+	
 
